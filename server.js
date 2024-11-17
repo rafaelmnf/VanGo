e5a145def995470bef4e834b40aa8ac932359054
rafaelmnf <rafaelnfilho@gmail.com>
2024-11-17 01:21:49 -0300

    otimização absurda de código (só faltou footer)

--- server.js
+++ server.js
@@ -6,18 +6,28 @@ const app = express();
 require('dotenv').config()
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public'))); // deixar com css
+app.use(express.static(path.join(__dirname, 'Public'))); // deixar com css
 
 
 //ROTA PARA MOSTRAR TELA INICIAL
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'public', 'HTML', 'index.html'));
+    res.sendFile(path.join(__dirname,'Public', 'HTML', 'index.html'));
   });
 
-
+//ROTA PARA MOSTRAR LOGIN
 app.get('/login',  (req, res) => {
-    res.sendFile(path.join(__dirname,'public', 'HTML', 'login.html'));
+    res.sendFile(path.join(__dirname,'Public', 'HTML', 'login.html'));
   });
+
+//ROTA PARA MOSTRAR REQUISIÇÃO DE LOGIN
+app.get('/request-login',  (req, res) => {
+  res.sendFile(path.join(__dirname,'Public', 'HTML', 'request-login.html'));
+});
+
+//ROTA PARA MOSTRAR LOGIN
+app.get('/maps',  (req, res) => {
+  res.sendFile(path.join(__dirname,'Public', 'HTML', 'maps.html'));
+});
 
 // ENVIO DE EMAILS
 // Configurando o nodemailer para enviar e-mails

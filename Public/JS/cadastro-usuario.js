b313ab415f981fbe53b3c3749a8d811b3d9a64fe
arturcontarelli <artur.contarelli@gmail.com>
2024-12-10 22:16:30 -0300

    ajustes

--- Public/JS/cadastro-usuario.js
+++ Public/JS/cadastro-usuario.js
@@ -1,29 +1,36 @@
-const form = document.getElementById('userForm');
 
-form.addEventListener('submit', async(event)=>{
-    event.preventDefault();
-    const nome = document.getElementById("nome").value;
-    const sobrenome = document.getElementById("sobrenome").value;
-    const email = document.getElementById("email").value;
-    const senha = document.getElementById("senha").value;
-    const cpf = document.getElementById("cpf").value;
-    
-    
-    const usuarioDados = {nome, sobrenome, email, senha, cpf};
-    console.log(usuarioDados);    
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("usuario-form");
 
-})
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
 
-async function criarUsuario(usuarioDados){
-    await fetch('/criar-usuario', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(usuarioDados)
+        const nome = document.getElementById("nome").value;
+        const sobrenome = document.getElementById("sobrenome").value;
+        const email = document.getElementById("email").value;
+        const senha = document.getElementById("senha").value;
+        const cpf = document.getElementById("cpf").value;
+        const usuarioDados = {nome, sobrenome, email, senha, cpf}
+
+        await criarUsuario(usuarioDados)
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
+});
+
+// Função para criar usuário
+async function criarUsuario(user) {
+    try {
+        const response = await fetch("/criar-usuario", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        });
+
+        const data = await response.json();
+        console.log("Resposta do servidor:", data);
+        alert(`Usuario: ${user.nome} criado com sucesso`)
+    } catch (error) {
+        console.error("Erro ao criar usuário:", error);
+    }
 }

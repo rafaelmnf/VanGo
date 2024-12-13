8101eca2294ecd8c1260194091e2e232aac09ca8
arturcontarelli <artur.contarelli@gmail.com>
2024-12-12 22:01:13 -0300

    a

---
+++ Public/JS/jwt/jwtconfig.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+
+function gerarToken(dadosUsuario)
+{
+    const secretKey = "djashduioqwrhdiu23ogr312iuG&!#¨#T!@&¨#T&@!bhcasasas"
+    const tempoExpira = "1h";
+    const payload = {
+        nome: dadosUsuario.nome,
+        email: dadosUsuario.email,
+        id: dadosUsuario.id
+    }
+    const token = jwt.sign(payload, secretKey, {expiresIn: tempoExpira})
+    payloads = [
+        payload.nome.id, 
+        token
+    ]
+    return payloads 
+}
+
+module.exports = {
+    gerarToken
+}
